b0fe181f036bd4ba251d1dc61297c0485cc351ca
bobacCurry <bobac.curry@gmail.com>
2019-12-17 00:39:12 +0800

    fix

--- src/api/share.js
+++ src/api/share.js
@@ -1,8 +1,22 @@
 import axios from '@/libs/api.request'
 
 export const getUserClient = () => {
-  return axios.request({
-    url: 'service/client/get_user_client',
-    method: 'get'
-  })
+  	return axios.request({
+    	url: 'service/client/get_user_client',
+    	method: 'get'
+  	})
+}
+
+export const sendCode = (phone,type) => {
+	return axios.request({
+	    url: `service/auth/send_code/${phone}/${type}`,
+	    method: 'post'
+	})
+}
+
+export const confirmCode = (phone,code) => {
+	return axios.request({
+	    url: `service/auth/confirm_code/${phone}/${code}`,
+	    method: 'post'
+	})
 }
--- src/view/share/client.vue
+++ src/view/share/client.vue
@@ -18,8 +18,8 @@
               </Select>
             </div>
             <div class="bind-item">
-              <Input v-model="newClient.phone" placeholder="Telegram账号 如：（63977***3224）">
-                <Button slot="append" style="width: 100px" v-if="newClient.timer" :disabled="newClient.timer>0">倒计时{{newClient.timer}}s</Button>
+              <Input v-model="newClient.phone" placeholder="TG账号（带国家区号）">
+                <Button slot="append" style="width: 100px" v-if="newClient.timer" :disabled="newClient.timer>0">已发送 {{newClient.timer}}s</Button>
                 <Button slot="append" style="width: 100px" @click="sendCode" v-else>发送验证码</Button>
               </Input>
             </div>
@@ -29,7 +29,7 @@
               </Input>
             </div>
             <div class="bind-item">
-              <Button @click="newClient.binding=false">取消绑定</Button>
+              <Button @click="newClient.binding=false" type="error">取消绑定</Button>
             </div>
           </div>
         </Card>
@@ -38,7 +38,7 @@
 	</div>
 </template>
 <script>
-import {getUserClient} from '@/api/share'
+import {getUserClient,sendCode,confirmCode} from '@/api/share'
 import client from '@/config/client'
 export default {
   data () {
@@ -51,45 +51,125 @@ export default {
         code:'',
         type:null,
         timer:0
-      }
+      },
+      countdown:null
     }
   },
   mounted () {
     this.getClient()
   },
   methods: {
+    
     getClient () {
+    
       getUserClient().then((r)=>{
+    
         if (r.data.success) {
+    
           this.clientList = r.data.msg
+    
         }
+    
       })
     },
     sendCode () {
+      
       if (this.newClient.type===null) {
+      
         return this.$Notice.error({title:"请选择服务类型"})
+      
       }
+      
       if (!this.newClient.phone.trim()) {
+      
         return this.$Notice.error({title:"请输入telegram账号"})
+      
       }
-      this.newClient.timer = 120
-      let countdown = setInterval(()=>{
-        this.newClient.timer--
-        if(this.newClient.timer === 0) {
-          clearInterval(countdown);
+      
+      sendCode(this.newClient.phone,this.newClient.type).then((r)=>{
+        
+        if (r.data.success) {
+
+          this.newClient.phone = ''
+
+          this.newClient.code = ''
+
+          this.newClient.type = null
+
+          this.newClient.binding=false
+
+          this.getClient()
+        
+          return this.$Notice.success({title:r.data.msg})
+        
+        }else{
+        
+          return this.$Notice.error({title:r.data.msg})
+        
         }
-      },1000)
+      
+      }).catch((e)=>{
+      
+        return this.$Notice.error({title:e.response.data.msg})      
+      
+      }).finally(()=>{
+      
+        this.stopCount()
+      })
+      
+      this.startCount()
     },
     confirmCode() {
       if (!this.newClient.phone.trim()) {
         return this.$Notice.error({title:"请输入telegram账号"})
       }
+
       if (!this.newClient.timer) {
         return this.$Notice.error({title:"请先发送验证码"})
       }
+      
       if (!this.newClient.code) {
         return this.$Notice.error({title:"请输入验证码"})
       }
+
+      confirmCode(this.newClient.phone,this.newClient.code).then((r)=>{
+        console.log(r.data)
+      })
+    },
+    cancelConfirm(){
+      
+      if (this.newClient.phone.trim()&&this.newClient.timer&&this.newClient.code) {
+      
+        confirmCode(this.newClient.phone,this.newClient.code).then((r)=>{
+      
+          console.log(r.data)
+      
+        }) 
+      }
+      
+      this.stopCount()
+    },
+    startCount(){
+
+      this.newClient.timer = 120
+      
+      this.countdown = setInterval(()=>{
+      
+        this.newClient.timer--
+      
+        if(this.newClient.timer === 0) {
+      
+          clearInterval(this.countdown)
+      
+        }
+      
+      },1000)
+    },
+    stopCount(){
+
+      this.newClient.timer = 0
+      
+      clearInterval(this.countdown)
     }
   }
 }
