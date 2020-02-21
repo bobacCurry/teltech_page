2c67cc54de7dd6f3bffc1dd33cd411b65e9b4e39
bobacCurry <bobac.curry@gmail.com>
2020-02-22 02:34:47 +0800

    fix

--- src/view/service/group.vue
+++ src/view/service/group.vue
@@ -147,8 +147,8 @@ export default{
 			if (String(this.order.text_type)==='1'&&!this.order.media.trim()) {
 				return this.$Notice.error({title:'请填写广告文本'})
 			}
-			if (this.order.chat.length>80) {
-				return this.$Notice.error({title:'最多选择80个群'})			
+			if (this.order.chat.length>100) {
+				return this.$Notice.error({title:'最多选择100个群'})			
 			}
 
 			this.loading = true
