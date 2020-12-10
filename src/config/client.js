ea15936621e5e71bdda603c45061a4740a0efa1f
alex <bobac.curry@gmail.com>
2020-12-10 13:09:41 +0800

    fix

--- src/config/client.js
+++ src/config/client.js
@@ -2,7 +2,7 @@ export const serviceType = ['群发服务','私信服务','拉群服务']
 	
 export const clientStatus = ['新绑定','正常','被禁言（spam）','被封禁（banned）','验证失效']
 
-export const chatType = ['工作信息','商业推广','美食外卖','其他业务1','其他业务2']
+export const chatType = ['工作信息','商业推广','美食外卖','短文专场','➖专场']
 
 export const textType = ['文字','图片']
 
