ff665b79b38be62ebaec04707312d26e3708f9d0
alex <bobac.curry@gmail.com>
2021-01-09 13:00:42 +0800

    fix

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="login">
+    <div class="warning">
+      本系统服务于海外华人（不支持中国大陆地区用户）外卖、二手、房产、交易等生活类信息发布，切勿发布虚假、违法、暴力、色情等违规信息，如有发现举报，封号处理！！！
+    </div>
     <div class="login-con">
       <Card icon="log-in" title="欢迎登录" :bordered="false">
         <div class="form-con">
